f4740934affde31b272d7f9c2ce82a509fcbcb53
gizemayegul <gizemayazyegul@gmail.com>
2022-12-09 19:52:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav.js';
+import Intro from './components/Intro.js';
+import Intro2 from './components/Intro2.js';
+import Intro3 from './components/Intro3.js';
+import Footer from './components/Footer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function Heading() { 
+  return ( 
+    <div class="heading">
+      <h1>This is my first React Project</h1> 
     </div>
-  );
-}
+  ) 
+} 
 
+
+function App() { 
+  return ( 
+    <div className="App"> 
+      I didn't work on that much,
+      <Heading /> 
+      <Nav /> 
+      <Intro />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+      
+    </div> 
+  ); 
+} 
+ 
 export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,16 @@
+function Intro(){
+    return(
+        <div className="blog-post-intro">
+            <h2>I've becoma a react developer Insallah!</h2>
+            <div>
+                <p>
+                    I completed React basics! I am happy to announce that i am Junior React Developer
+                </p>
+                <p classname="link">Read more..,</p>
+            </div>
+        </div>
+
+    );
+};
+
+export default Intro;
